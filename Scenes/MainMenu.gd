904daf3348f47ce741be84210ead1a2c02e28535
Your Name <you@example.com>
2024-02-16 12:06:59 -0800

    initial mockup of main menu screen

---
+++ Scenes/MainMenu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_play_pressed():
+	#load in first level
+	pass # Replace with function body.
+
+
+func _on_settings_pressed():
+	#load settings page
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	#exit game
+	pass # Replace with function body.
