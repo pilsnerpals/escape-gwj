47b12e9fcc629214be71c0fbf45da037b435abc3
Alex Johnson <alex.johnson293@gmail.com>
2024-02-16 20:53:19 -0800

    remove player checks

--- Scenes/LevelManager.gd
+++ Scenes/LevelManager.gd
@@ -7,20 +7,15 @@ extends Node
 ## @tutorial(TitleHere):		https://theurl.com
 ## @experimental or ## @deprecated
 
-# can set up do:while for loading/transitional elements (death/reset/menu to level/etc)
 # I have yet to review gdscript syntax
-# another pattern would be to create a container that holds 'global' vars between levels (worlds within worlds)
-
 var level_number = 1
 var screen_size = Vector2()
 var is_switching_level = false
 
 func _ready():
 	## sets screen size and loads current level
-	##
-	
-	##
-	screen_size = 100
+	## screen size should probably be handled elsewhere
+	screen_size = 300
 	load_level(level_number)
 
 func load_level(level):
@@ -29,6 +24,9 @@ func load_level(level):
 	var level_instance = level_scene.instantiate()
 	add_child(level_instance)
 	is_switching_level = false
+	
+	var player = get_node("Node2D/Player")
+	var entry_pos = Vector2(100,100)
 
 func switch_level():
 	if is_switching_level:
@@ -36,26 +34,10 @@ func switch_level():
 
 	is_switching_level = true
 	level_number += 1
-	#what
-	#if level_number > 2:
-		#level_number = 1
 
 	# fetch player node "Player"
 	var player = get_node("Node2D/Player")
 	
-	if player:
-		# create position variable
-		var player_position = player.position
-
-		if player_position.x < 0:
-			player.position.x = screen_size.x
-		elif player_position.x > screen_size.x:
-			player.position.x = 0
-		elif player_position.y < 0:
-			player.position.y = screen_size.y
-		elif player_position.y > screen_size.y:
-			player.position.y = 0
-
 	#unload old 2DNode level
 	get_node("Node2D").queue_free()
 	load_level(level_number)
@@ -66,10 +48,7 @@ func _process(delta):
 
 	if player:
 		var player_position = player.position
-		var condition1 = player_position.x < 0
-		var condition2 = player_position.x > screen_size.x
-		var condition3 = player_position.y < 0
-		var condition4 = player_position.y > screen_size.y
+		var level_complete = player_position.x > 500
 
-		if condition1 or condition2 or condition3 or condition4 :
+		if level_complete
 			switch_level()
