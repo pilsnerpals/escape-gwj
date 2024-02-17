8b45dda96f99b32e374f0fad9061cd742bd32ee2
nickwhy <nick.weivoda@gmail.com>
2024-02-16 19:55:27 -0800

    pulling updated main

---
+++ Scenes/game_play.gd
@@ -0,0 +1,25 @@
+extends Node
+
+@onready var bgm = [$bgm1, $bgm2, $bgm3]
+var bgm_current = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$bgm1.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_bgm_1_finished():
+	$bgm2.play()
+
+
+func _on_bgm_2_finished():
+	$bgm3.play()
+
+
+func _on_bgm_3_finished():
+	$bgm1.play()
