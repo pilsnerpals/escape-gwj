908db923bdc9d989242f463719c31bfca1ee71d4
Your Name <you@example.com>
2024-02-17 23:06:39 -0800

    updated the exit scene to be its own thing, added level switching to the gameplay script

--- Scenes/game_play.gd
+++ Scenes/game_play.gd
@@ -5,10 +5,19 @@ extends Node
 var bgm_current = 0
 @onready var coffees
 @onready var coworkers
-
+#put in the level length
+const levelLengths = [5920, 15120]
+var currentLevel = 0
+var canSwitch = true
+@onready var currentLevelScene = $Level0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$bgm1.play()
+	handleLevelLoad(currentLevel)
+	# hard coded for level 0 need to figure out how to deal with this a better way
+	
+
+func handleLevelLoad(levelNum: int):
 	coffees = get_tree().get_nodes_in_group("Coffees")
 	coworkers = get_tree().get_nodes_in_group("Coworkers")
 	get_tree().call_group("coworkers", "init")
@@ -16,9 +25,28 @@ func _ready():
 		c.collected.connect(%Player._on_coffee_collected)
 	for w in coworkers:
 		w.player_hit.connect(%Player._on_co_worker_player_hit)
+	var exits = get_tree().get_nodes_in_group("Exit")
+	for e in exits:
+		e.player_exited.connect(_onExitReached)
+	%ProgressBar.setMaxLength(levelLengths[levelNum])
+	
+func _onExitReached(level: int):
+	if canSwitch:
+		%LevelSwitchTimer.start()
+		canSwitch = false
+		%Player.position = Vector2(57, 721)
+		currentLevel += 1;
+		#load new scene
+		var level_path = "res://Scenes/Level" + str(currentLevel) + ".tscn"
+		var level_scene = load(level_path)
+		var level_instance = level_scene.instantiate()
+		#this is temporary
 		
-	# hard coded for level 0 need to figure out how to deal with this a better way
-	%ProgressBar.setMaxLength(5920) 
+		add_child(level_instance)
+		#unload old scene
+		currentLevelScene.queue_free()
+		currentLevelScene = level_scene
+		handleLevelLoad(currentLevel)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -37,3 +65,8 @@ func _on_bgm_2_finished():
 
 func _on_bgm_3_finished():
 	$bgm1.play()
+
+
+func _on_level_switch_timer_timeout():
+	canSwitch = true
+	pass # Replace with function body.
